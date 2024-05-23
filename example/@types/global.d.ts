52ea34c380e361b3ce230a522b665c9a08081193
Denis Angell <dangell@transia.co>
2024-05-23 10:05:33 +0200

    update objects

--- example/@types/global.d.ts
+++ example/@types/global.d.ts
@@ -1,61 +1,131 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { Transaction } from '@transia/xrpl-models'
-import { STObject } from './objects'
+import {
+  Transaction,
+  TransactionMetadata,
+  CreatedNode,
+  DeletedNode,
+  ModifiedNode,
+  PreviousFields,
+  FinalFields,
+  NewFields,
+  Memo,
+  SignerEntry,
+  NFToken,
+  EmitDetails,
+  Hook,
+  Signer,
+  Majority,
+  DisabledValidator,
+  // EmittedTxn,
+  HookExecution,
+  HookDefinition,
+  HookParameter,
+  HookGrant,
+  ActiveValidator,
+  ImportVLKey,
+  HookEmission,
+  MintURIToken,
+  AmountEntry,
+  GenesisMint,
+  // Misc
+  IssuedCurrency,
+  XRP,
+  Amount,
+  IssuedCurrencyAmount,
+} from "@transia/xrpl-models";
+
+export type PreviousFields = { [field: string]: unknown };
+export type FinalFields = { [field: string]: unknown };
+export type NewFields = { [field: string]: unknown };
+
+export type STObject =
+  // Objects (IN ORDER)
+  | TransactionMetadata
+  | CreatedNode
+  | DeletedNode
+  | ModifiedNode
+  | PreviousFields
+  | FinalFields
+  | NewFields
+  | Memo
+  | SignerEntry
+  | NFToken
+  | EmitDetails
+  | Hook
+  | Signer
+  | Majority
+  | DisabledValidator
+  // | EmittedTxn
+  | HookExecution
+  | HookDefinition
+  | HookParameter
+  | HookGrant
+  | ActiveValidator
+  | ImportVLKey
+  | HookEmission
+  | MintURIToken
+  | AmountEntry
+  | GenesisMint
+  // Misc
+  | IssuedCurrency
+  | XRP
+  | Amount
+  | IssuedCurrencyAmount;
 
 declare global {
   /********************************************************************************************************************* */
 
   // Return number: int64
   //    An arbitrary return code you wish to return from your hook. This will be present in the metadata of the originating transaction.
-  export type Hook = (reserved?: number) => bigint /** int64 */
+  export type Hook = (reserved?: number) => bigint; /** int64 */
 
   // Arg: uint32 'what'
   //    0 = the emittted transaction to which this callback relates was successfully accepted into a ledger.
   //    1 = the emitted transaction to which the callback relates was NOT successfully accepted into a ledger before it expired.
   // Return number: int64
   //    An arbitrary return code you wish to return from your hook. This will be present in the metadata of the originating transaction.
-  export type Callback = (emittedTxError?: number) => bigint /** int64 */
+  export type Callback = (emittedTxError?: number) => bigint; /** int64 */
 
   /********************************************************************************************************************* */
 
-  type Trace = (message: string, data: any, hex?: boolean) => void
+  type Trace = (message: string, data: any, hex?: boolean) => void;
 
-  const trace: Trace
+  const trace: Trace;
 
   /********************************************************************************************************************* */
 
   // Rtrn
-  type Accept = (error_msg: string, error_code: number) => bigint
+  type Accept = (error_msg: string, error_code: number) => bigint;
 
-  const accept: Accept
-
-  /********************************************************************************************************************* */
-
-  type Rollback = (error_msg: string, error_code: number) => bigint
-
-  const rollback: Rollback
+  const accept: Accept;
 
   /********************************************************************************************************************* */
 
-  const util_raddr = (arg: any) => any
-  const util_accid = (arg: any) => any
-  const util_sha512h = (arg: any) => any
-  const hook_account = (arg?: any) => any
-  const state = (arg: any) => any
-  const hook_hash = (arg: any) => any
-  const state_set = (arg: any, arg: any) => any
-  const otxn_type = () => any
+  type Rollback = (error_msg: string, error_code: number) => bigint;
 
-  type OTXNTransaction = () => Transaction
-  const otxn_json: OTXNTransaction
+  const rollback: Rollback;
 
-  type PrepareTransaction = (txn: Transaction) => any
-  const prepare: PrepareTransaction
+  /********************************************************************************************************************* */
 
-  type STOToJson = () => STObject
-  const sto_to_json: STOToJson
+  const util_raddr = (arg: any) => any;
+  const util_accid = (arg: any) => any;
+  const util_sha512h = (arg: any) => any;
+  const hook_account = (arg?: any) => any;
+  const state = (arg: any) => any;
+  const hook_hash = (arg: any) => any;
+  const state_set = (arg: any, arg: any) => any;
+  const otxn_type = () => any;
+
+  type OTXNTransaction = () => Transaction;
+  const otxn_json: OTXNTransaction;
+
+  type PrepareTransaction = (txn: Transaction) => any;
+  const prepare: PrepareTransaction;
+
+  type STOToJson = () => STObject;
+  const sto_to_json: STOToJson;
 }
 
-export {}
+export {};
--- example/contracts/index.ts
+++ example/contracts/index.ts
@@ -1,20 +1,11 @@
-import { myCustom } from "./utils/custom"
-import { DOESNT_EXIST, ttINVOKE } from "jshooks-api"
+import { SignerEntry } from "../@types/objects"
 
 const Hook = (arg: any) => {
   trace('HookOnTT.js: Called.', false)
-  // _g(1, 1)
-
-  const isThis = myCustom("this")
-
-  const tt = otxn_type()
-  const txn = otxn_json()
-  trace(txn.Account, 1)
-  if (tt !== ttINVOKE) {
-    return rollback('hook_on_tt: HookOn field is incorrectly set.', DOESNT_EXIST)
-  }
+  const obj = sto_to_json() as unknown as SignerEntry
+  obj.SignerEntry.Account
   return accept('HookOnTT.js: Finished.', 13)
 }
 
-// PURGED ON BUILD
+// REQUIRED FOR ESBUILD
 export { Hook }
