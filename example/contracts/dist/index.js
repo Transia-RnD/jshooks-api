46d84ed3fb2d5a8cd52521042950aa3c9b057987
Denis Angell <dangell@transia.co>
2024-05-23 10:07:13 +0200

    Update index.js

--- example/contracts/dist/index.js
+++ example/contracts/dist/index.js
@@ -1,19 +1,6 @@
-var myCustom = (thisis) => {
-  return thisis == "that";
-};
-
-var DOESNT_EXIST = -5;
-
-var ttINVOKE = 99;
-
 var Hook = (arg) => {
   trace("HookOnTT.js: Called.", false);
-  const isThis = myCustom("this");
-  const tt = otxn_type();
-  const txn = otxn_json();
-  trace(txn.Account, 1);
-  if (tt !== ttINVOKE) {
-    return rollback("hook_on_tt: HookOn field is incorrectly set.", DOESNT_EXIST);
-  }
+  const obj = sto_to_json();
+  obj.SignerEntry.Account;
   return accept("HookOnTT.js: Finished.", 13);
 };
