ffbed4796f22de69fbd30a2a7b121d53f5596686
Denis Angell <dangell@transia.co>
2024-06-20 10:09:22 +0200

    fix example

--- example/contracts/dist/index.js
+++ example/contracts/dist/index.js
@@ -1,3 +1,5 @@
+var DOESNT_EXIST = -5;
+
 var Hook = (arg) => {
   const tx = otxn_json();
   if (tx.TransactionType === "Payment" && tx.Account === "rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn") {
--- example/contracts/index.ts
+++ example/contracts/index.ts
@@ -1,6 +1,5 @@
 import { Payment } from "@transia/xahau-models";
 import { DOESNT_EXIST } from "jshooks-api";
-import { accept, otxn_json, rollback } from "jshooks-api";
 
 const Hook = (arg: any) => {
   const tx = otxn_json() as Payment;
