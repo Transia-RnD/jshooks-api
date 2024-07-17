e11e51f77a4a7f9054038581da184cc0738a8767
Denis Angell <dangell@transia.co>
2024-07-17 07:41:08 +0200

    remove transaction types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -168,42 +168,7 @@ declare global {
   /**
    * Return the Transaction Type of the originating transaction
    *
-   * @returns Transaction Type:
-   *                  ttPAYMENT:	0
-   *                  ttESCROW_CREATE:	1
-   *                  ttESCROW_FINISH:	2
-   *                  ttACCOUNT_SET:	3
-   *                  ttESCROW_CANCEL:	4
-   *                  ttREGULAR_KEY_SET:	5
-   *                  ttOFFER_CREATE:	7
-   *                  ttOFFER_CANCEL:	8
-   *                  ttTICKET_CREATE:	10
-   *                  ttTICKET_CANCEL:	11
-   *                  ttSIGNER_LIST_SET:	12
-   *                  ttPAYCHAN_CREATE:	13
-   *                  ttPAYCHAN_FUND:	14
-   *                  ttPAYCHAN_CLAIM:	15
-   *                  ttCHECK_CREATE:	16
-   *                  ttCHECK_CASH:	17
-   *                  ttCHECK_CANCEL:	18
-   *                  ttDEPOSIT_PREAUTH:	19
-   *                  ttTRUST_SET:	20
-   *                  ttACCOUNT_DELETE:	21
-   *                  ttHOOK_SET:	22
-   *                  ttURITOKEN_MINT:	45
-   *                  ttURITOKEN_BURN:	46
-   *                  ttURITOKEN_BUY:	47
-   *                  ttURITOKEN_CREATE_SELL_OFFER:	48
-   *                  ttURITOKEN_CANCEL_SELL_OFFER:	49
-   *                  ttGENESIS_MINT:	96
-   *                  ttIMPORT:	97
-   *                  ttCLAIM_REWARD:	98
-   *                  ttINVOKE:	99
-   *                  ttAMENDMENT:	100
-   *                  ttFEE:	101
-   *                  ttUNL_MODIFY:	102
-   *                  ttEMIT_FAILURE:	103
-   *                  ttUNL_REPORT:	104
+   * @returns number as Transaction Type
    */
   const otxn_type: () => number
 
