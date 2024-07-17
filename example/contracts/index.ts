a92ffa4f14fae0e3c6b78c3f17abb18fa697d13f
Denis Angell <dangell@transia.co>
2024-07-17 08:02:37 +0200

    update example

--- example/contracts/index.ts
+++ example/contracts/index.ts
@@ -1,17 +1,23 @@
-import { Payment } from "@transia/xahau-models";
-import { DOESNT_EXIST } from "jshooks-api";
+import { Payment } from '@transia/xahau-models'
+import { DOESNT_EXIST } from 'jshooks-api'
+import { myCustom } from './utils/custom'
 
 const Hook = (arg: any) => {
-  const tx = otxn_json() as Payment;
+  const tx = otxn_json() as Payment
+  if (typeof tx === 'number' && tx === DOESNT_EXIST) {
+    rollback('', DOESNT_EXIST)
+  }
   if (
-    tx.TransactionType === "Payment" &&
-    tx.Account === "rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn"
+    tx.TransactionType === 'Payment' &&
+    tx.Account === 'rG1QQv2nh2gr7RCZ1P8YYcBUKCCN633jCn'
   ) {
-    return rollback("", DOESNT_EXIST);
+    rollback('Blocked Payment', 0)
   }
 
-  return accept('', 13);
-};
+  myCustom('some')
+
+  accept('', 13)
+}
 
 // REQUIRED FOR ESBUILD
-export { Hook };
+export { Hook }
